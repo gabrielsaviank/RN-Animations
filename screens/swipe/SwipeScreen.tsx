89379eafdd002e344d232ab3abf6bbf38f113fb7
gabrielsavian <gabrielsvnzrd@gmail.com>
2021-12-13 22:01:56 -0300

    feat: deck card generated, must fix images

--- screens/swipe/SwipeScreen.tsx
+++ screens/swipe/SwipeScreen.tsx
@@ -4,11 +4,12 @@ import {
     StyleSheet,
     Text
 } from "react-native";
+import { Card, Button } from 'react-native-elements';
 
 import Deck from './components/Deck';
 
 const DATA = [
-    { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+    { id: 1, text: 'Card #1', uri: 'https://www.destructoid.com/wp-content/uploads/2021/09/Lost-in-Random-Shadowman-screenshot.jpg' },
     { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
     { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
     { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
@@ -21,9 +22,20 @@ const DATA = [
 class SwipeScreen extends React.Component<any, any> {
     renderCard(item: any) {
         return (
-            <Text>
-                {item.text}
-            </Text>
+            <Card
+                key={item.id}
+                title={item.text}
+                image={{uri: item.uri}}
+            >
+                <Text style={{marginBottom: 10}}>
+                    I Can  customise the card further
+                </Text>
+                <Button
+                    icon={{name: 'code'}}
+                    backgroundColor="#03A9F4"
+                    title="View Now!"
+                />
+            </Card>
         );
     };
 
