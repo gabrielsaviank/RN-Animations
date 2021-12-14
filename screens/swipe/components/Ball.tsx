ee128bd7e2375a5f1fb8ab09c6944c8fc58ec6c2
gabrielsavian <gabrielsvnzrd@gmail.com>
2021-12-13 21:28:33 -0300

    feat: created deck and added data

--- screens/swipe/components/Ball.tsx
+++ screens/swipe/components/Ball.tsx
@@ -2,15 +2,31 @@ import React, { Component } from 'react';
 import {
     View,
     StyleSheet,
+    Animated
 } from "react-native";
 
-class Ball extends Component {
+interface IBallProps {
+    position: any
+}
+
+class Ball extends React.Component<IBallProps> {
+    componentWillMount() {
+        this.position = new Animated.ValueXY(0, 0);
+        // @ts-ignore
+        Animated.spring(this.position, {
+            toValue: { x: 200, y: 500}
+        }).start();
+    }
+
     render(){
         return (
-            <View style={styles.ball}/>
-        )
+            <Animated.View style={this.position.getLayout()}>
+                <View style={styles.ball}/>
+            </Animated.View>
+        );
     }
 }
+
 const styles = StyleSheet.create({
     ball: {
         height: 60,
---
+++ screens/swipe/components/Deck.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Animated } from "react-native";
+
+class Deck extends Component<any, any> {
+    render(){
+        return (
+            <View>
+
+            </View>
+        );
+    }
+};
+
+export default Deck;
