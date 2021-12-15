4c961ebb0318a94ad9e2c77883b33af0a6d9ee63
gabriel zardo <gabriel.zardo@magrathealabs.com>
2021-12-15 18:49:57 -0300

    feat: force swipe added

--- screens/swipe/components/Deck.tsx
+++ screens/swipe/components/Deck.tsx
@@ -8,6 +8,7 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250
 
 class Deck extends Component<any, any> {
     constructor(props) {
@@ -22,16 +23,27 @@ class Deck extends Component<any, any> {
             },
             onPanResponderRelease: (event, gesture) => {
                 if(gesture.dx > SWIPE_THRESHOLD){
-                    console.log("swipe right")
+                    // @ts-ignore
+                    this.forceSwipe('right');
                 } else if (gesture.dx < -SWIPE_THRESHOLD) {
-                    console.log('swipe left')
+                    this.forceSwipe('left')
                 } else {
                     this.resetPosition();
                 }
             }
         });
+
         this.state = { panResponder,position };
     }
+
+    forceSwipe(direction){
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+            useNativeDriver: false,
+            toValue: {x, y: 0},
+            duration: SWIPE_OUT_DURATION
+        }).start()
+    };
 
     resetPosition() {
         // @ts-ignore
